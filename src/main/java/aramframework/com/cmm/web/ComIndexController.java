e0ad78decd37300cafb120211a2670bbf53cebd9
Hunchul Cho <hnccho@hanafos.com>
2020-04-02 12:55:53 +0900

    메인페이지 수정

--- src/main/java/aramframework/com/cmm/web/ComIndexController.java
+++ src/main/java/aramframework/com/cmm/web/ComIndexController.java
@@ -11,12 +11,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller; 
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import aramframework.com.cmm.annotation.IncludedInfoVO;
-import aramframework.com.cmm.domain.AramKomoranResult;
-import aramframework.com.cmm.service.KomoranService;
-import kr.co.shineware.nlp.komoran.model.KomoranResult;
 import aramframework.com.cmm.annotation.IncludedInfo;
 
 /**
@@ -40,50 +36,13 @@ public class ComIndexController {
 
 	@Autowired ApplicationContext applicationContext;
 
-	@Autowired 
-	private KomoranService komoranService; 
-
 	protected final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
 	private Map<Integer, Object> map;
 
 	@IncludedInfo(name = "초기화면", order = 1000, gid = 10)
 	@RequestMapping("/UnitContent.do")
-	public String unitContent(
-			@RequestParam(value="inputText", required=false) String inputText, 
-			ModelMap model) {
-
-		if( inputText == null || "".equals(inputText) ) {
-			inputText = "산림청에서 근무해 왔으며, 슬하에 아들 한 명을 두고 있다.";
-		}
-		
-		KomoranResult komoranResultList = komoranService.analyze(inputText);
-
-		AramKomoranResult aramResultList = new AramKomoranResult(komoranResultList);
-		
-		//print each tokens by getTokenList()  
-//		List<Token> tokenList = analyzeResultList.getTokenList();
-//		KORMORLOG.debug("==========print 'getTokenList()'==========");
-//		for (Token token : tokenList) {
-//			KORMORLOG.debug(token.toString()); 
-//			KORMORLOG.debug(token.getMorph()+"/"+token.getPos()+"("+token.getBeginIndex()+","+token.getEndIndex()+")");
-//			KORMORLOG.debug("");
-//		}
-
-		//print nouns
-//		KORMORLOG.debug("==========print 'getNouns()'==========");
-//		KORMORLOG.debug(analyzeResultList.getNouns().toString());
-//		KORMORLOG.debug("");
-
-//		KORMORLOG.debug("==========print 'getPlainText()'==========");
-//		KORMORLOG.debug(analyzeResultList.getPlainText());
-//		KORMORLOG.debug("");
-
-//		KORMORLOG.debug("==========print 'getList()'==========");
-//		KORMORLOG.debug(analyzeResultList.getList().toString());
-		model.addAttribute("inputText", inputText);
-		model.addAttribute("resultText", aramResultList.getPlainText());
-			
+	public String unitContent() {
 		return "cmm/UnitContent";
 	}
 	

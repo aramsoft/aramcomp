37f142985733a2544ae02db30b1893f375eed440
hnccho <hnccho@hanafos.com>
2026-01-27 12:41:04 +0900

    패키지 이동

--- src/main/java/aramframework/cmm/security/userdetails/manager/DaoUserDetailsManager.java
+++ src/main/java/aramframework/cmm/security/userdetails/manager/DaoUserDetailsManager.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package aramframework.cmm.security.userdetails.dao;
+package aramframework.cmm.security.userdetails.manager;
 
 import java.util.ArrayList;
 import java.util.Collection;
